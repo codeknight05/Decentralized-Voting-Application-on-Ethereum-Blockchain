c28cf7e3706d35e0e76f6526ffb2a05c6f1a728c
Yash Khandelwal <khandelwalyash.ajay2022@vitstudent.ac.in>
2025-04-10 18:04:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const ethers = require('ethers');
 
 var port = 3000;
 
+
 const API_URL = process.env.API_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
